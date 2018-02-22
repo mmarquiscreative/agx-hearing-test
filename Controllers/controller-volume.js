9797f03195a9c15a1c68d0b96fdd4829efd12ac6
David Kerr <dkerr@agcreativemac07.audigygroup.local>
2018-02-21 16:51:09 -0800

    gets volume stage functional, aside from noise not stopping

--- Controllers/controller-volume.js
+++ Controllers/controller-volume.js
@@ -1,3 +1,15 @@
 angular.module('formApp').controller('volumeController', ['$scope', function($scope) {
   var volume = this;
-}])
+
+  var noise = new Audio('/sounds/noise.wav');
+  
+  noise.currentTime = 0;
+  noise.loop = true;
+  // commenting out until I can get it to stop when you leave this stage
+  //noise.play();
+
+  function stopNoise() {
+    noise.loop = false;
+    noise.stop();
+  };
+}]);
