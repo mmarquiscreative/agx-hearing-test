ba2a98ea0ccae7e7cb56ec204e33cdf795700162
mmarquiscreative <mmarquis.creative@gmail.com>
2018-09-10 14:41:40 -0700

    Corrected sound location

--- Templates/stage-volume.php
+++ Templates/stage-volume.php
@@ -5,7 +5,7 @@
         <p ng-hide="volume.tonePlaying" class="lead-text">Click the button below to begin playing the audio. (Headphones recommended)</p>
         <p ng-show="volume.tonePlaying" class="lead-text">Adjust the volume of your computer to play at a comfortable level. This will establish a baseline volume for the rest of the quiz. <br/><br/>Click the 'Next Step' button when ready.</p>
     </div>
-     <audio id="volumeAudio" src="noise.mp3" loop autoplay></audio>
+     <audio id="volumeAudio" ng-src="/wp-content/plugins/agx-hearing-test/sounds/noise.mp3" loop src="/wp-content/plugins/agx-hearing-test/sounds/noise.mp3" autoplay></audio>
          <div class="btns-yn">
      <button ng-hide="volume.tonePlaying" ng-click="volume.startNoise()" ui-class="btn-yn-vol-tone"> Play Audio<span class="glyphicon glyphicon-circle-arrow-right"></span></button>
     	<button ng-show="volume.tonePlaying" ng-click="volume.stopNoise()" ui-sref="stage.toneTest" class="btn-yn-vol-submit">Next Step <span class="glyphicon glyphicon-circle-arrow-right"></span></button>
