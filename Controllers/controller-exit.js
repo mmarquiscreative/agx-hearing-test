0048251210248c74bc784af0058a01c288324763
David Kerr <davallerr@gmail.com>
2018-09-14 22:14:03 -0700

    starts adjustment for modal

--- Controllers/controller-exit.js
+++
@@ -1,6 +0,0 @@
-angular.module('formApp').controller('exitController', ['$scope', function($scope) {
-  var exit = this;
-
-  
-
-}]);
--- agx-hearing-test.php
+++ agx-hearing-test.php
@@ -14,22 +14,22 @@ require_once 'scripts.php';
 
 function agx_hearing_test($content) {
   echo '
-    <div id="agx-hearing-test" ng-app="formApp">
-      <div class="container" ng-controller="stageController as stage">
+    <div id="agx-ohq" ng-app="formApp">
+      <h2>Some blurb about the AGX Online Hearing Quiz</h2>
+      <button class="btn-ohq-modal" ng class="pop.modalBtn" ng-click="pop.updateDisplay()">Take The Quiz</button>
+      <div id="ohq-modal" ng-controller="modalController as pop">
         <button ui-sref="stage.exit" ng-class="stage.testBool(&#39;results&#39;) ? &#39;btn-exit&#39; : &#39;hidden&#39;">X</button>
-        <button ng-click="" ng-class="stage.testBool(&#39;results&#39;) ? &#39;hidden&#39; : &#39;btn-exit&#39;">X</button>
+        <button ng-click="pop.updateDisplay()" ng-class="stage.testBool(&#39;results&#39;) ? &#39;hidden&#39; : &#39;btn-exit&#39;">X</button>
 
-        <!-- views will be injected here -->
-        <div ui-view></div>
+        <div class="ohq-container" ng-controller="stageController as stage">
 
+          <!-- views will be injected here -->
+          <div ui-view></div>
+
+        </div>
       </div>
     </div>
   ';
-
 }
 
 add_filter('wp_footer','agx_hearing_test');
-
-
-
-
