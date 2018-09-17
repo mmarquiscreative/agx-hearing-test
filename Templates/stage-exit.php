d318ddfe1f11b81eb741afc1708b94ca2ec78e41
mmarquiscreative <mmarquis.creative@gmail.com>
2018-09-17 15:03:21 -0700

    PHP Form Test

--- Templates/stage-exit.php
+++ Templates/stage-exit.php
@@ -3,7 +3,7 @@
     <p class="lead-text">Not ready to speak to a hearing expert?</p>
     <p class="lead-text">Get your hearing quiz results emailed to you and learn more about better hearing.</p>
 
-
+<?php echo do_shortcode('[ninja_form id=1]'); ?>
     <input type="email" placeholder="example@email.com">
 
     <button ng-click="exit.reportSubmit()" class="btn-submit">Send My Results</button>
--- agx-hearing-test.php
+++ agx-hearing-test.php
@@ -11,7 +11,8 @@ define ("DIR", __FILE__);
 require_once 'scripts.php';
 // require_once 'wp/template.php';
 
-
+<h2>Testing Testing Testing</h2>
+    <?php echo do_shortcode('[ninja_form id=1]'); ?>
 function agx_hearing_test($content) {
   echo '
     <div id="agx-ohq" ng-app="formApp">
