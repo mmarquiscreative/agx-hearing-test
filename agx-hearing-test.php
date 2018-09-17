6c1f060f9b4b87376639aa41b5db792d072aacac
mmarquiscreative <mmarquis.creative@gmail.com>
2018-09-17 15:06:38 -0700

    undo last change

--- agx-hearing-test.php
+++ agx-hearing-test.php
@@ -11,11 +11,9 @@ define ("DIR", __FILE__);
 require_once 'scripts.php';
 // require_once 'wp/template.php';
 
-<h2>Testing Testing Testing</h2>
-    <?php echo do_shortcode('[ninja_form id=1]'); ?>
+
 function agx_hearing_test($content) {
-  echo '
-    <div id="agx-ohq" ng-app="formApp">
+  echo '<div id="agx-ohq" ng-app="formApp">
       <h2>Some blurb about the AGX Online Hearing Quiz</h2>
       
       <div ng-controller="modalController as pop">
