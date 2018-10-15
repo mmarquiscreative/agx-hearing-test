09456de569ff85eef90f6a40689264112a3cddca
mmarquiscreative <mmarquis.creative@gmail.com>
2018-10-15 11:10:56 -0700

    AGX Hearing top menu override

--- Controllers/controller-stage.js
+++ Controllers/controller-stage.js
@@ -1,57 +1,117 @@
 // CONTROLLERS
 angular.module('formApp').controller('stageController', ['$scope', '$state', 'resultsObj', '$location', function ($scope,  $state, resultsObj, $location) {
-    var stage = this;
-	
+    var stage, headerEl, headerStyles, themePageStyles, starterZIndex;
+    stage = this;
+
     /* START: Modal Functionality */
-		stage.cta_text = document.querySelector('#cta_text').textContent;
-		stage.cta_url = document.querySelector('#cta_url').textContent;
-	
-		resultsObj.cta_text = stage.cta_text;
-		resultsObj.cta_url = stage.cta_url;
-	
-        // Modal Classes
-        stage.modalClass = 'ohq-modal';
-        stage.modalBtnOpen = 'btn-open';
-        stage.modalBtnClose = 'btn-close';
-        
-        // Node
-        stage.modal = document.querySelector('.ohq-modal');
+    stage.cta_text = document.querySelector('#cta_text').textContent;
+    stage.cta_url = document.querySelector('#cta_url').textContent;
 
-        
-        // Toggle between stage-up and button
-        stage.updateDisplay = function() {
-            console.log('Taesting ' + stage.modalClass);
-            
-            var testClass = stage.modalClass;
+    resultsObj.cta_text = stage.cta_text;
+    resultsObj.cta_url = stage.cta_url;
 
-            if(testClass === 'ohq-modal') {
-				/* document.querySelector('#ohq-container').style.display = 'block'; */
-                				$state.go('stage.intro');
+    // Modal Classes
+    stage.modalClass = 'ohq-modal';
+    stage.modalBtnOpen = 'btn-open';
+    stage.modalBtnClose = 'btn-close';
 
-				stage.modalClass = 'ohq-modal-active';
-				
-                console.log('ohq-modal ===> ohq-modal-active');
-				
-            } else if(testClass === 'ohq-modal-active') {
-				/* document.querySelector('#ohq-container').style.display = 'hidden'; */
-                stage.modalClass = 'ohq-modal';
-				
-                console.log('ohq-modal-active ===> ohq-modal');
-                				$state.go('stage.intro');
-resultsObj.restartTest();
-            } else {
-                console.log('No match. Current style is ' + stage.modalClass);
-            };
-        };
-
-
-        /* END: Modal Functionality */
-
+    // Node
+    stage.modal = document.querySelector('.ohq-modal');
 
     
+    
+    ///////////////////////////////////////////////////////////////
+    // ::AGXHearing.com::START AGX Hearing Floating Header Workaround 
+    ///////////////////////////////////////////////////////////////
+    
+    headerEl = document.querySelector('.mk-header');
+    headerStyles = window.getComputedStyle(headerEl, null);
+
+    themePageEl = document.querySelector('#theme-page');
+    themePageStyles = window.getComputedStyle(themePageEl, null);
+    starterZIndex = headerStyles.zIndex;
+    
+    console.log(headerEl);
+    
+    ///////////////////////////////////////////////////////////////
+    // ::AGXHearing.com::END AGX Hearing Floating Header Workaround 
+    ///////////////////////////////////////////////////////////////    
+
+    
+    
+    
+    // Toggle between stage-up and button
+    stage.updateDisplay = function() {
+        var testClass, themePageZIndex, tempHeaderIndex;
+        
+        testClass = stage.modalClass;
+        themePageZIndex = themePageStyles.zIndex;
+        tempHeaderIndex = (themePageZIndex - 1);
+
+        console.log('headerEL is: ' + headerEl +
+            'starterZIndex: ' + starterZIndex +
+            '\nthemePageZIndex: ' + themePageZIndex + 
+            '\ntempHeaderIndex: ' + tempHeaderIndex);
+
+        if(testClass === 'ohq-modal') {
+            /* document.querySelector('#ohq-container').style.display = 'block'; */
+            $state.go('stage.intro');
+
+            stage.modalClass = 'ohq-modal-active';
+
+            console.log('ohq-modal ===> ohq-modal-active');
+
+            
+            
+        ///////////////////////////////////////////////////////////////
+        // ::AGXHearing.com::START AGX Hearing Floating Header Workaround 
+        ///////////////////////////////////////////////////////////////
+
+            if(headerEl){
+                headerEl.style.zIndex = tempHeaderIndex;
+            } else {
+                console.log('headerEl returned false. HeaderEl was ' + headerEl);
+            };
+            
+        ///////////////////////////////////////////////////////////////
+        // ::AGXHearing.com::END AGX Hearing Floating Header Workaround 
+        ///////////////////////////////////////////////////////////////
+
+        } else if(testClass === 'ohq-modal-active') {
+            /* document.querySelector('#ohq-container').style.display = 'hidden'; */
+            stage.modalClass = 'ohq-modal';
+
+            console.log('ohq-modal-active ===> ohq-modal');
+            $state.go('stage.intro');
+            resultsObj.restartTest();
+        
+        ///////////////////////////////////////////////////////////////
+        // ::AGXHearing.com::START AGX Hearing Floating Header Workaround 
+        ///////////////////////////////////////////////////////////////
+            
+            if(headerEl){
+                console.log(starterZIndex);
+                headerEl.style.zIndex = starterZIndex;
+            } else {
+                console.log('headerEl returned false. HeaderEl was ' + headerEl);
+            };
+            
+        ///////////////////////////////////////////////////////////////
+        // ::AGXHearing.com::END AGX Hearing Floating Header Workaround 
+        ///////////////////////////////////////////////////////////////
+        } else {
+            console.log('No match. Current style is ' + stage.modalClass);
+        };
+    };
+
+
+    /* END: Modal Functionality */
+
+
+
     stage.testBool = function(stageName){
         var returnBool;
-        
+
 
 
 
@@ -86,13 +146,13 @@ resultsObj.restartTest();
 
     stage.restart = function(){
         resultsObj.restartTest();
-		$state.go('stage.intro');
+        $state.go('stage.intro');
     };
-	
-	/* stage.loadForm = function(){
-		
+
+    /* stage.loadForm = function(){
+
 	console.log('running test form');
-   
+
 	var resultsMessage = document.querySelector(".nf-quiz-message");
 	var quizResults = document.querySelector(".nf-hearing-results");
 	var toneResults = document.querySelector(".nf-tone-results");
@@ -102,12 +162,12 @@ resultsObj.restartTest();
     };
 	console.log(resultsMessage);
 		var newValue = '<p style="background: blue; width: 70%; padding: 1rem;" >70%</p>';
-		
+
        jQuery( '.nf-hearing-results' ).val( newValue ).trigger( 'change' );
-    
-   
-	
+
+
+
 		console.log(resultsMessage.value);
 	} */
-	
+
 }])
